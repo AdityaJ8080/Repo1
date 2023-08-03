6656d18de280850cb2389bf379468b9902617b7a
Admin <Admin@DESKTOP-IE060VU>
2023-08-03 20:39:51 +0530

    Updated Float.

--- src/Assignment/Temperature.java
+++ src/Assignment/Temperature.java
@@ -5,14 +5,13 @@ import java.util.Scanner;
 public class Temperature {
 
 	public static void main(String[] args) {
+		
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter Temp");
-		int i = scan.nextInt();
-		
-		
-		System.out.println(i + " Celsius = " + (i+273.15) + " Kelvin");
-		
+		System.out.println("Enter Temperature in Celsius");
+		float i = scan.nextFloat();
 	
+		System.out.println(i + " Celsius = " + (i+273.15) + " Kelvin");
+			
 		System.out.println(i + " Celsius = " + (i+32) + " Fehrenheit");
 
 	}
